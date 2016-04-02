55737ea11d23ed139163ac4caabf7b21dc54917f
root <root@ip-172-31-6-154.us-west-1.compute.internal>
2016-04-02 13:16:36 +0000

    Updated deploy script

--- deployment/stage.sh
+++ deployment/stage.sh
@@ -4,9 +4,20 @@
 
 echo "Doing a staging deployment"
 
-cp -R "$WORKSPACE" /tmp/stage
-chown -R meteor:meteor /tmp/stage
-sudo -u meteor -H bash -c "mv /tmp/stage /home/meteor/staging; cd /home/meteor/staging; /usr/local/bin/meteor meteor --settings settings.json --release 1.2.1 --port 4000" 
+# Run whatever tests we want (none for now).
 
-#Should be running on http://localhost:4000
+# Things are good so far so make is so that the meteor user can read the files
+echo "Make the workspace files accessible"
+chmod -R 777 "$WORKSPACE"
+
+# As the meteor user clean up and stage files
+echo "Run as meteor user"
+sudo -u meteor -H bash -c "rm -rf /home/meteor/staging; mkdir -p /home/meteor/staging_tmp; cp -R '$WORKSPACE' /home/meteor/staging_tmp; mv /home/meteor/staging_tmp/workspace /home/meteor/staging; rmdir /home/meteor/staging_tmp"
+
+# Run the deployment 
+echo "Spwaning meteor process..."
+sudo -u meteor -H bash -c "cd /home/meteor/staging; /usr/local/bin/meteor --settings settings.json --release 1.2.1 --port 4000 >> /home/meteor/staging.log 2>&1 &" 
+
+# Should be running on http://localhost:4000
+#TODO Figure out a way to verify this!
 
