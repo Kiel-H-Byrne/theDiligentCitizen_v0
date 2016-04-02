1254c0fd1bb5f410cd28bdf2e4b8f85cb42d5e88
root <root@ip-172-31-6-154.us-west-1.compute.internal>
2016-04-02 15:09:06 +0000

    Added prod script and made changes to both so they kill the existing running job.

---
+++ deployment/prod.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script shoudl be run in hudson by doing bash -ex stage.sh
+
+echo "Doing a staging deployment"
+
+# Run whatever tests we want (none for now).
+
+# Things are good so far so make is so that the meteor user can read the files
+echo "Make the workspace files accessible"
+chmod -R 777 "$WORKSPACE"
+
+# Kill any existing running meteor staging apps
+echo "Kill meteor apps"
+pkill -f "/home/meteor/.meteor" -U meteor
+
+# As the meteor user clean up and stage files
+echo "Run as meteor user"
+sudo -u meteor -H bash -c "rm -rf /home/meteor/prod; mkdir -p /home/meteor/prod_tmp; cp -R '$WORKSPACE' /home/meteor/prod_tmp; mv /home/meteor/prod_tmp/workspace /home/meteor/prod; rmdir /home/meteor/prod_tmp"
+
+# Run the deployment 
+echo "Spwaning meteor process..."
+sudo -u meteor -H bash -c "cd /home/meteor/prod; /usr/local/bin/meteor --settings settings.json --release 1.2.1 --port 5000 >> /home/meteor/prod.log 2>&1 &" 
+
+# Should be running on http://localhost:5000
+#TODO Figure out a way to verify this!
+
--- deployment/stage.sh
+++ deployment/stage.sh
@@ -10,13 +10,17 @@ echo "Doing a staging deployment"
 echo "Make the workspace files accessible"
 chmod -R 777 "$WORKSPACE"
 
+# Kill any existing running meteor staging apps
+echo "Kill meteor staging apps"
+pkill -f "/home/meteor_stage/.meteor" -U meteor_stage
+
 # As the meteor user clean up and stage files
 echo "Run as meteor user"
-sudo -u meteor -H bash -c "rm -rf /home/meteor/staging; mkdir -p /home/meteor/staging_tmp; cp -R '$WORKSPACE' /home/meteor/staging_tmp; mv /home/meteor/staging_tmp/workspace /home/meteor/staging; rmdir /home/meteor/staging_tmp"
+sudo -u meteor_stage -H bash -c "rm -rf /home/meteor_stage/staging; mkdir -p /home/meteor_stage/staging_tmp; cp -R '$WORKSPACE' /home/meteor_stage/staging_tmp; mv /home/meteor_stage/staging_tmp/workspace /home/meteor_stage/staging; rmdir /home/meteor_stage/staging_tmp"
 
 # Run the deployment 
 echo "Spwaning meteor process..."
-sudo -u meteor -H bash -c "cd /home/meteor/staging; /usr/local/bin/meteor --settings settings.json --release 1.2.1 --port 4000 >> /home/meteor/staging.log 2>&1 &" 
+sudo -u meteor_stage -H bash -c "cd /home/meteor_stage/staging; /usr/local/bin/meteor --settings settings.json --release 1.2.1 --port 4000 >> /home/meteor_stage/staging.log 2>&1 &" 
 
 # Should be running on http://localhost:4000
 #TODO Figure out a way to verify this!
