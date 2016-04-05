0ddee82f8d6364d375aad4aa3d05837644e98dce
KHB <kiel.byrne@gmail.com>
2016-04-05 14:02:24 -0400

    trying an if else for each division level

--- client/imports/api/polTree.js
+++ client/imports/api/polTree.js
@@ -14,5 +14,15 @@ Template.polTree.helpers({
 		if (data) {
 			return data.officials[index][property];
 		}
+	},
+	newRow: function(did) {
+//TODO: Need to get how many 'levels' there are to the org chart, 
+
+		//get current value of 'divisionId' & compare it to current version.
+		//if different, create a new <'ul'> for it, otherwise leave it be.
+		//return true if different
+
+
+
 	}
 });
