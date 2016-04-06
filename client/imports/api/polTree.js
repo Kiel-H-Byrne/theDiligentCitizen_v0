54d8fedcf20eb37a3583d82b2e9f37f5422d9750
KHB <kiel.byrne@gmail.com>
2016-04-06 01:03:14 -0400

    corrected template logic for listed jquery chart

--- client/imports/api/polTree.js
+++ client/imports/api/polTree.js
@@ -12,6 +12,7 @@ Template.polTree.onRendered( function() {
 Template.polTree.helpers({
 	getOfficial: function(property, data, index) {
 		if (data) {
+			console.log(property, data, index);
 			return data.officials[index][property];
 		}
 	},
@@ -21,8 +22,19 @@ Template.polTree.helpers({
 		//get current value of 'divisionId' & compare it to current version.
 		//if different, create a new <'ul'> for it, otherwise leave it be.
 		//return true if different
-
-
-
+		var lastDid = Session.get('curDiv');
+		var now = this.divisionId;
+		console.log(now);
+		var last = lastDid;
+		if (now === last) {
+			var newRow = true;
+			return newRow;
+		}
+		else {
+			newRow = false;
+			console.log(newRow);			
+			//Session.set('curDiv', now);
+			return newRow;
+		}
 	}
 });
