dc443b1c8a5730761429b1fb0ce1c4ccfcf41a2b
KHB <kiel.byrne@gmail.com>
2016-05-07 01:03:00 -0400

    houstonadmin *sigh*

--- server/startup/collections.js
+++ server/startup/collections.js
@@ -6,3 +6,24 @@
 // 	return UserInfo.find();
 // });
 
+var id = Accounts.createUser({
+    email: "kiel.byrne@gmail.com",
+    password: "vivitron",
+    profile: { name: "KHB" }
+});
+
+Roles.addUsersToRoles(id, ['admin'], 'default-group');
+
+
+if (Meteor.isServer) {
+	Meteor.startup( function() {
+		Posts = new Meteor.Collection('posts');
+
+		AdminConfig = { 
+			adminEmails: ['	kiel.byrne@gmail.com '], 
+			collections: { 
+				Posts: {} 
+			}
+		}
+	});
+}
